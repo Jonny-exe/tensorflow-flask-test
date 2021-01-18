5351d969ac2fce1585f552753aae12a79bba0bc0
Jonny-exe <69385502+Jonny-exe@users.noreply.github.com>
2021-01-18 15:46:50 +0100

    Test with python working

--- src/index.py
+++ src/index.py
@@ -39,7 +39,7 @@ def input_fn(features, labels, training=True, batch_size=256):
 my_feature_columns = []
 for key in train.keys():
     my_feature_columns.append(tf.feature_column.numeric_column(key=key))
-print(my_feature_columns)
+print("my feature columns: ", my_feature_columns)
 
 
 classifier = tf.estimator.DNNClassifier(

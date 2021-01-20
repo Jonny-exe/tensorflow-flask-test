9fd0049be140760825fc64664db29de9fe5c9723
Jonny-exe <69385502+Jonny-exe@users.noreply.github.com>
2021-01-20 21:52:32 +0100

    Added all the functions for the flask server

--- src/sever/httpd/db.py
+++ src/sever/httpd/db.py
@@ -5,9 +5,9 @@ import mariadb
 # Connect to MariaDB Platform
 try:
     conn = mariadb.connect(
-        user="code_share",
+        user="code-share",
         password="password",
-        host="192.0.2.1",
+        host="127.0.0.1",
         port=3306,
         database="code_share",
     )
@@ -17,3 +17,22 @@ except mariadb.Error as e:
 
 # Get Cursor
 cur = conn.cursor()
+
+def get_current_likes(id):
+    cur.execute("select likes from messages where id=?", (id,))
+    return cur.fetchone()[0]
+
+def get_messages():
+    cur.execute("select text, likes, id from messages limit 30")
+    return cur.fetchall()
+
+
+def add_like(id, newLikes):
+    cur.execute("update messages set likes=? where id=?", (newLikes,id))
+
+def get_current_files(id):
+    cur.execute("select likes from messages where id=?", (id))
+    return cur.fetchone()[0]
+        
+def insert_message(text):
+    cur.execute("insert into messages(text,likes) values(?,?)",(text, 0))
--- src/sever/httpd/index.py
+++ src/sever/httpd/index.py
@@ -1,23 +1,66 @@
 # index.py
 from flask import Flask, jsonify, request
-import csvFuncs as funcs
-# from csvFuncs import add_row
+import csv_funcs as csv_f
+
+# import tensor_funcs as tensor_f
+import db
+
 app = Flask(__name__)
 
+print("Hello")
 
-incomes = [
-    {'description': 'salary', 'amount': 5000}
-]
+incomes = [{"description": "salary", "amount": 5000}]
 
 
 @app.route("/")
 def hello_world():
     return "Hello, World!"
 
-@app.route('/add_row', methods=['POST'])
+
+@app.route("/add_row", methods=["POST"])
 def add_income():
     json = request.get_json()
-    funcs.add_row(dict(json))
+    csv_f.add_row(dict(json))
     return json, 200
 
-    
+
+@app.route("/get_current_likes", methods=["POST"])
+def get_current_likes():
+    req = request.json
+    db_result = db.get_current_likes(req["id"])
+    print(db_result)
+    result = {"current_likes": db_result}
+    return result
+
+
+@app.route("/insert_message", methods=["POST"])
+def insert_message():
+    req = request.json
+    db.insert_message(req["text"])
+    return {"status": 200}
+
+
+@app.route("/get_messages")
+def get_messages():
+    db_result = db.get_messages()
+    final_result = []
+    items_in_result = ["text", "likes", "id"]
+    for result in db_result:
+        final_item = {}
+
+        for item in range(len(items_in_result)):
+            final_item[items_in_result[item]] = result[0]
+
+        final_result.append(final_item)
+
+    return {"messages": final_result}
+
+
+@app.route("/add_like", methods=["POST"])
+def add_like():
+    req = request.json
+    id = req['id']
+    newLikes = db.get_current_likes(id) + 1
+    print(newLikes)
+    db.add_like(id, newLikes)
+    return {'status': 200}
